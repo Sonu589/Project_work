aaa293ad63df89011a0f4caf28eb89b3406f4f87
Sonu <78155991+Sonu589@users.noreply.github.com>
2023-07-17 01:08:32 +0530

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,95 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import Ajax from "./tech/Ajax.png"
+import Angular from "./tech/Angular.png"
+import AWS from "./tech/AWS.png"
+import Express from "./tech/Express.png"
+import Jenkins from "./tech/Jenkins.png"
+import Jquery from "./tech/Jquery.png"
+import Kubernetes from "./tech/Kubernetes.png"
+import Linux from "./tech/Linux.png"
+import Mysql from "./tech/Mysql.png"
+import Sql from "./tech/Sql.png"
+
+import Coding from "./company/Coding.png"
+import Edunet from "./company/Edunet.png"
+import Hack2skill from "./company/Hack2skill.png"
+import Microsoft from "./company/Microsoft.png"
+import Social from "./company/Social.png"
+import Zuno from "./company/Zuno.png"
+
+import Employee from "./Employee.png"
+import Quiz from "./Quiz.png"
+import Portfolio from "./Portfolio.png"
+import Square from "./Square.png"
+import Weather from "./Weather.png"
+import Hotel from "./Hotel.png"
+import Chat from "./Chat.png"
+import CPP from "./Cpp.png"
+import Clone from "./Clone.png"
+
+export {
+  Employee,
+  Quiz,
+  Portfolio,
+  Square,
+  Weather,
+  Hotel,
+  Chat,
+  CPP,
+  Clone,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  Ajax,
+  Linux,
+  Kubernetes,
+  Jenkins,
+  Jquery,
+  Mysql,
+  Sql,
+  Angular,
+  AWS,
+  Express,
+
+  Hack2skill,
+  Coding,
+  Edunet,
+  Microsoft,
+  Social,
+  Zuno,
+};
